5e760cba7612c44261e7327231e9d45b6a501bb4
STE <STE@DESKTOP-E7U49CV>
2024-08-11 20:32:27 +0530

    secound github java program

---
+++ java_project_for_push_github/src/java_project_for_push_github/secound_github.java
@@ -0,0 +1,10 @@
+package java_project_for_push_github;
+
+public class secound_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello gittu");
+	}
+
+}
